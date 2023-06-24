d67df5e1018bd13cf14902a2dace3ddabde2cb9e
kawashima <kawashima@cloudpack.jp>
2023-06-25 01:54:32 +0900

    Updates

--- infra/lib/asset/lambda/pipeline-trigger/main.go
+++ infra/lib/asset/lambda/pipeline-trigger/main.go
@@ -28,7 +28,7 @@ type CodeCommitDetail struct {
 	ReferenceType              string `json:"referenceType"`
 	ReferenceName              string `json:"referenceName"`
 	ReferenceFullName          string `json:"referenceFullName"`
-	CommitID                   string `json:"commitId"`
+	CommitId                   string `json:"commitId"`
 	OldCommitId                string `json:"oldCommitId"`
 	BaseCommitId               string `json:"baseCommitId"`
 	SourceCommitId             string `json:"sourceCommitId"`
@@ -38,10 +38,42 @@ type CodeCommitDetail struct {
 	ConflictResolutionStrategy string `json:"conflictResolutionStrategy"`
 }
 
-func HandleRequest(ctx context.Context, event events.CloudWatchEvent) {
+func getChangedFiles(ctx context.Context, cfg *aws.Config, repositoryName string, oldCommitId string, commitId string) ([]string, error) {
+	client := codecommit.NewFromConfig(*cfg)
+
+	resp, err := client.GetDifferences(ctx, &codecommit.GetDifferencesInput{
+		RepositoryName:        aws.String(repositoryName),
+		BeforeCommitSpecifier: aws.String(oldCommitId),
+		AfterCommitSpecifier:  aws.String(commitId),
+	})
+	if err != nil {
+		log.Printf("Error getting differences: %v", err)
+	}
+
+	var paths []string
+	for _, diff := range resp.Differences {
+		paths = append(paths, *diff.AfterBlob.Path)
+	}
+
+	return paths, nil
+}
+
+func startPipeline(ctx context.Context, cfg *aws.Config, pipelineName string) {
+	client := codepipeline.NewFromConfig(*cfg)
+
+	resp, err := client.StartPipelineExecution(ctx, &codepipeline.StartPipelineExecutionInput{
+		Name: aws.String(pipelineName),
+	})
+	if err != nil {
+		log.Printf("Error starting pipeline: %v", err)
+	}
+	log.Printf("Success started pipeline: %s, executionId: %s\n", pipelineName, *resp.PipelineExecutionId)
+}
+
+func handleRequest(ctx context.Context, event events.CloudWatchEvent) {
 	pipelinesJSON := os.Getenv("PIPELINES")
 	if pipelinesJSON == "" {
-		log.Fatalf("PIPELINES environment variable is not set")
+		log.Fatalf("Error environment variable PIPELINES not set")
 	}
 
 	var pipelines []PipelineInfo
@@ -61,36 +93,23 @@ func HandleRequest(ctx context.Context, event events.CloudWatchEvent) {
 		log.Fatalf("Error loading SDK configuration: %v", err)
 	}
 
-	ccClient := codecommit.NewFromConfig(cfg)
-	cpClient := codepipeline.NewFromConfig(cfg)
+	files, err := getChangedFiles(context.TODO(), &cfg, detail.RepositoryName, detail.OldCommitId, detail.CommitId)
+	if err != nil {
+		log.Fatalf("Error getting changed files: %v", err)
+	}
 
 	pipelinePrefix := strings.Split(os.Getenv("AWS_LAMBDA_FUNCTION_NAME"), "pipeline-handler")[0]
 
 	for _, pipeline := range pipelines {
-
-		changes, err := ccClient.GetDifferences(ctx, &codecommit.GetDifferencesInput{
-			RepositoryName:        aws.String(detail.RepositoryName),
-			BeforeCommitSpecifier: aws.String(detail.OldCommitId),
-			AfterCommitSpecifier:  aws.String(detail.CommitID),
-		})
-		if err != nil {
-			log.Printf("Error getting differences: %v", err)
-			continue
-		}
-
-		pipelineName := pipelinePrefix + pipeline.Name + "-pipeline"
-		if len(changes.Differences) > 0 {
-			_, err := cpClient.StartPipelineExecution(ctx, &codepipeline.StartPipelineExecutionInput{
-				Name: aws.String(pipelineName),
-			})
-			if err != nil {
-				log.Printf("Error starting pipeline %s: %v", pipelineName, err)
+		for _, file := range files {
+			pipelineName := pipelinePrefix + pipeline.Name + "-pipeline"
+			if strings.HasPrefix(file, pipeline.Path) {
+				startPipeline(context.TODO(), &cfg, pipelineName)
 			}
-			log.Printf("Started pipeline: %s", pipelineName)
 		}
 	}
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	lambda.Start(handleRequest)
 }
