d64937c09e7125a63c975d2cb24d8b0a932d3d94
kawashima <kawashima@cloudpack.jp>
2023-06-26 14:06:00 +0900

    Updates

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,10 +3,23 @@ import App from "./App";
 
 test("renders data from API", async () => {
   const mockResponse = {
-    lambdaVersion: "5",
-    apiVersion: "v1",
-    resource: "item1",
+    handler: "",
+    defaultRegion: "",
+    executionEnv: "",
+    functionName: "",
+    functionVersion: "",
+    memorySize: "",
+    InitializationType: "",
+    logGroupName: "",
+    taskRoot: "",
+    runtimeDir: "",
+    lang: "",
+    path: "",
+    nodePath: "",
+    tz: "",
+    event: {},
   };
+
   global.fetch = jest.fn(() =>
     Promise.resolve({
       json: () => Promise.resolve(mockResponse),
@@ -17,9 +30,20 @@ test("renders data from API", async () => {
 
   await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1));
 
-  await screen.findByText(/lambdaVersion/);
-  await screen.findByText(/apiVersion/);
-  await screen.findByText(/resource/);
+  await screen.findByText(/handler/);
+  await screen.findByText(/defaultRegion/);
+  await screen.findByText(/executionEnv/);
+  await screen.findByText(/functionName/);
+  await screen.findByText(/functionVersion/);
+  await screen.findByText(/memorySize/);
+  await screen.findByText(/InitializationType/);
+  await screen.findByText(/logGroupName/);
+  await screen.findByText(/runtimeDir/);
+  await screen.findByText(/lang/);
+  await screen.findByText(/path/);
+  await screen.findByText(/nodePath/);
+  await screen.findByText(/tz/);
+  await screen.findByText(/event/);
 
   jest.clearAllMocks();
 });
